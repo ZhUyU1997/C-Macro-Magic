3a8e19136994b7b55f8dd71edd797975ea53a021
yzhu <891085309@qq.com>
2020-12-13 17:35:40 +0800

    [Format]

--- printv.h
+++ printv.h
@@ -6,9 +6,9 @@
 #define CAT(x, y) PRIMITIVE_CAT(x, y)
 
 #define __IS_EMPTY(a, b, size, ...) size
-#define _IS_EMPTY(...) __IS_EMPTY(__VA_OPT__(,), 0, 1)
+#define _IS_EMPTY(...) __IS_EMPTY(__VA_OPT__(, ), 0, 1)
 #define IS_EMPTY(...) _IS_EMPTY(__VA_ARGS__)
- 
+
 #define EMPTY()
 #define DEFER(id) id EMPTY()
 
@@ -19,17 +19,13 @@
 #define EVAL4(...) EVAL5(EVAL5(EVAL5(__VA_ARGS__)))
 #define EVAL5(...) __VA_ARGS__
 
-#define _FOR_EACH(macro, x, ...)           \
-    CAT(_FOR_EACH_, IS_EMPTY(__VA_ARGS__)) \
-    (macro, x, __VA_ARGS__)
+#define _FOR_EACH(macro, x, ...) CAT(_FOR_EACH_, IS_EMPTY(__VA_ARGS__))(macro, x, __VA_ARGS__)
 #define _FOR_EACH_0(macro, x, ...) macro(x) DEFER(_FOR_EACH_I)()(macro, __VA_ARGS__)
 #define _FOR_EACH_1(macro, x, ...) macro(x)
 #define _FOR_EACH_I() _FOR_EACH
 #define FOR_EACH(macro, ...) EVAL(_FOR_EACH(macro, __VA_ARGS__))
 
-#define PRINT_FUNC(fmt, type) \
-    type:                     \
-    print_##fmt
+#define PRINT_FUNC(fmt, type) type:print_##fmt
 
 #if 0
 #define PRINT_TEMPLATE(fmt, type)                   \
@@ -53,7 +49,7 @@ static void print_p(void *i)
 }
 #endif
 
-#define PRINT_0(x) _Generic(            \
+#define PRINT_0(x) _Generic(          \
     (x),                              \
     PRINT_FUNC(s, char *),            \
     PRINT_FUNC(d, int),               \
@@ -63,15 +59,15 @@ static void print_p(void *i)
     PRINT_FUNC(p, default))(x);
 
 // #define PRINT_0(x)  x;
-#define PRINT_1(x)  
-#define __PRINTV(x) CAT(PRINT_,IS_EMPTY(x))(x)
-#define _PRINTV_0(...) ({ FOR_EACH(__PRINTV, ##__VA_ARGS__) })
+#define PRINT_1(x)
+#define __PRINTV(x) CAT(PRINT_, IS_EMPTY(x))(x)
+#define _PRINTV_0(...) ({FOR_EACH(__PRINTV, ##__VA_ARGS__)})
 #define _PRINTV_1(...)
 
-#define _PRINTV(...)                      \
+#define _PRINTV(...)                     \
     CAT(_PRINTV_, IS_EMPTY(__VA_ARGS__)) \
     (__VA_ARGS__)
-#define printv(...)  _PRINTV(__VA_ARGS__) 
+#define printv(...) _PRINTV(__VA_ARGS__)
 #define $(x) , x,
 
 PRINT_TEMPLATE(s, char *);
